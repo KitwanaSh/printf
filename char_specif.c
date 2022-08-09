a129cbec3aafd420d2594f9b5b67c0fb192b2130
chiderafaith <faithjosephs114@gmail.com>
2022-08-09 05:45:20 -0700

    update to specif

--- char_specif.c
+++ char_specif.c
@@ -89,3 +89,32 @@ int _print_str_nonprintable(va_list args)
 	}
 	return (count);
 }
+/**
+ *
+ *  * _print_str_reverse - prints a string in reverse
+ *
+ *   * @args: arguments list
+ *
+ *    *
+ *
+ *     * Return: number of chars printed
+ *
+ *      */
+
+int _print_str_reverse(va_list args)
+
+{
+	char *str;
+	int i, count = 0;
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
