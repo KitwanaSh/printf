95678fc8fa7b913c14005442d36cb5d61f864f47
chiderafaith <faithjosephs114@gmail.com>
2022-08-09 03:40:16 -0700

    update to main

---
+++ main.h
@@ -0,0 +1,8 @@
+printer _get_printer(const char *specifier);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _print_char(va_list);
+int _print_str(va_list);
+int _print_decimal(va_list);
+int _print_int(va_list);
+int _print_binary(va_list);
